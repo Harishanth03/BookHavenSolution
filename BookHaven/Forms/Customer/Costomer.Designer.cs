33358038b44eca81a0f925500bfb72ad0caf918f
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-06 17:54:43 +0530

    Add Costomer resource and UI components
    Updated `BookHaven.csproj` to include `Costomer.resx` as an embedded resource linked to `Costomer.cs`. Enhanced `Costomer.Designer.cs` by adding two labels with defined properties and layout methods for proper rendering. Created a new resource file `Costomer.resx` with XML schema and metadata for resource management.

--- BookHaven/Forms/Customer/Costomer.Designer.cs
+++ BookHaven/Forms/Customer/Costomer.Designer.cs
@@ -28,10 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(829, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1538, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Costomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Costomer";
+            this.Size = new System.Drawing.Size(1924, 1023);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
